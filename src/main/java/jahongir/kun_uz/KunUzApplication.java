8f6836746500554dac4d63c97c56bc03dd4463c9
ziyodullayevj7 <ziyodullayevj7@gmail.com>
2026-05-10 13:20:45 +0500

    first commit

---
+++ src/main/java/jahongir/kun_uz/KunUzApplication.java
@@ -0,0 +1,13 @@
+package jahongir.kun_uz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KunUzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KunUzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jahongir/kun_uz/KunUzApplicationTests.java
@@ -0,0 +1,13 @@
+package jahongir.kun_uz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KunUzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
